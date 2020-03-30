b6b10aceebe3e144697ead0919106ff1cfec48d2
landru27 <landru729@gmail.com>
2020-03-29 21:31:07 -0400

    add WebSocket logic to stats service

--- src/app/stats.service.ts
+++ src/app/stats.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs/Rx';
+import { WebsocketService } from './websocket.service';
+
+const STATS_URL = 'ws://127.0.0.1:8080/';
+
+export interface StockpileStats {
+  wheat: number;
+  flour: number;
+  bread: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatsService {
+  public messages: Subject<StockpileStats>;
 
-  constructor() { }
+  constructor(wsService: WebsocketService) {
+    this.messages = <Subject<StockpileStats>>wsService.connect(STATS_URL).map(
+      (response: MessageEvent): StockpileStats => {
+        let data = JSON.parse(response.data);
+        return {
+          wheat: data.wheat,
+          flour: data.flour,
+          bread: data.bread
+        };
+      }
+    );
+  }
 }
